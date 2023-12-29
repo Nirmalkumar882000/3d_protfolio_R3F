665357ae984d7d9f0e5c776d4be3a2b30c0ee07f
Lucky_World <128409984+Nirmalkumar882000@users.noreply.github.com>
2023-12-29 18:09:30 +0530

    dfgh

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,40 @@
-import React, { Suspense } from 'react'
-import { OrbitControls } from '@react-three/drei'
-import { Avatar } from './Avatar'
-
+import React, { Suspense } from "react";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 function Experience() {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
+
   return (
-   <>
-   <OrbitControls/>
-   <ambientLight intensity={2}/>
-   <Suspense fallback={null}>
-   <group position-y={-1} >
-      <Avatar/>
-   </group>
-   </Suspense>
-   </>
-  )
+    <>
+      <OrbitControls />
+      <ambientLight intensity={2} />
+      <Environment preset="sunset" />
+      <group position-y={-1}>
+
+      <ContactShadows opacity={0.50} scale={10} blur={1} far={10} resolution={256} color="#000000" />
+
+        <Avatar animation={animation} />
+        {animation === "Typing" && (
+          <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+            <boxGeometry />
+            <meshStandardMaterial color={"white"} />
+          </mesh>
+        )}
+
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+          <planeGeometry />
+          <meshStandardMaterial color={"white"} />
+        </mesh>
+      </group>
+    </>
+  );
 }
 
-export default Experience
+export default Experience;
